e4cff74c307b0dbf737a67e97e0df5071c07a69f
aryamadhavi03 <aryammadhavi@gmail.com>
2024-02-09 09:39:19 +0530

    h

---
+++ profile.py
@@ -0,0 +1,16 @@
+
+
+
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+
+# Set the window title
+window.title("My Window")
+
+# Set the window size
+window.geometry("400x300")
+
+# Display the window
+window.mainloop()
